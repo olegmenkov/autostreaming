05e80bb2f7dacdee16d4c85924b6b912fda3c50a
imgusevim <156576780+imgusevim@users.noreply.github.com>
2024-03-09 14:21:54 +0300

    Update private_handlers.py

--- tg-bot/private_handlers.py
+++ tg-bot/private_handlers.py
@@ -93,14 +93,13 @@ async def send_data_to_calendar(message, state):
         "port_obs": stream_data['port'],
         "password_obs": stream_data['password'],
     }
-    params = create_params(stream_data)
    
     # body = {"params" :{"start": stream_data['date1'], "stop": stream_data['date2'], "YT_server": stream_data['youtube_server'],
     #         "key_obs": stream_data['key'],
     #         "name": stream_data['name'], "ip_obs": stream_data['ip'], "port_obs": stream_data['port'],
     #         "password_obs": stream_data['password']}}
     url = "https://crm.miem.tv/telegram/calendar/create"
-    response = requests.post(url, json = params)
+    response = requests.post(url, json = stream_data)
     logger.info(f'Sent {str(params)} and received {str(response.status_code)}')
 
     if response.status_code == 200:
