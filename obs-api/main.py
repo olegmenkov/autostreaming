54b3e813eb66be2c464584ff348154c61a1a1005
gplorovg <gplorovg@gmail.com>
2024-04-26 23:17:23 +0300

    await conductor

--- obs-api/main.py
+++ obs-api/main.py
@@ -457,7 +457,7 @@ async def set_scene_handler(request_body: SetSceneModel):
     # obsclient = await conductor.get_obs_client(request_body.user_id, request_body.obs_name)
     # так больше не делаем
 
-    ip, port, password = conductor.get_obs_info(request_body.user_id, request_body.obs_name)
+    ip, port, password = await conductor.get_obs_info(request_body.user_id, request_body.obs_name)
 
     # if not await ping_obs(ip, port, password):
     #     return JSONResponse(status_code=409,
